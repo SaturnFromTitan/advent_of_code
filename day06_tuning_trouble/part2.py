5a91b1a8ec60ba29f38cf7872c5272ce67251fda
Martin Winkel <martin.winkel.pps@gmail.com>
2022-12-07 11:28:58 +0100

    forgot to push part2 of day6

--- day06_tuning_trouble/part2.py
+++ day06_tuning_trouble/part2.py
@@ -5,15 +5,20 @@ def main():
         buffer = f.read().strip()
 
     idx = 0
-    length = 4
+    length = 14
+    is_found = False
     for idx in range(len(buffer)):
         sub_buffer = buffer[idx:idx + length]
         unique_chars = set(sub_buffer)
         if len(unique_chars) == length:
+            is_found = True
             break
-    answer = idx + length
 
-    print(f"THE ANSWER IS: {answer}")
+    if is_found:
+        answer = idx + length
+        print(f"THE ANSWER IS: {answer}")
+    else:
+        print("Didn't find a a matching substring")
 
 
 if __name__ == '__main__':
