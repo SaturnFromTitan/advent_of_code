ffd5074128fc48dc2cbd0aea1f1d22d27b5c2253
Martin Winkel <martin.winkel.pps@gmail.com>
2022-12-09 14:37:24 +0100

    solution for second part

---
+++ day09_rope_bridge/part2.py
@@ -0,0 +1,100 @@
+from dataclasses import dataclass
+
+
+class Rope:
+    def __init__(self, number_of_knots):
+        self.knots = tuple(Knot() for _ in range(number_of_knots))
+
+    def move_one_step(self, direction):
+        previous_knot = None
+        for knot in self.knots:
+            if not previous_knot:
+                knot.move(direction)
+            else:
+                knot.follow(previous_knot)
+            previous_knot = knot
+
+    def get_locations(self):
+        return [k.get_location() for k in self.knots]
+
+
+@dataclass
+class Knot:
+    x: int = 0
+    y: int = 0
+
+    def move(self, direction):
+        if direction == "R":
+            self.x += 1
+        elif direction == "L":
+            self.x -= 1
+        elif direction == "U":
+            self.y += 1
+        elif direction == "D":
+            self.y -= 1
+        else:
+            raise ValueError(f"Receive unexpected direction '{direction}'")
+
+    def follow(self, other: "Knot"):
+        delta_y = other.y - self.y
+        delta_x = other.x - self.x
+        if abs(delta_y) <= 1 and abs(delta_x) <= 1:
+            # head & tail are still touching
+            return
+
+        # follow vertically
+        if delta_x == 0:
+            if delta_y > 0:
+                self.y += 1
+            elif delta_y < 0:
+                self.y -= 1
+            return
+
+        # follow horizontally
+        if delta_y == 0:
+            if delta_x > 0:
+                self.x += 1
+            elif delta_x < 0:
+                self.x -= 1
+            return
+
+        # follow diagonally
+        if delta_x > 0 and delta_y > 0:
+            self.x += 1
+            self.y += 1
+        elif delta_x > 0 and delta_y < 0:
+            self.x += 1
+            self.y -= 1
+        elif delta_x < 0 and delta_y < 0:
+            self.x -= 1
+            self.y -= 1
+        elif delta_x < 0 and delta_y > 0:
+            self.x -= 1
+            self.y += 1
+
+    def get_location(self):
+        return self.x, self.y
+
+
+def main():
+    with open("input.txt") as f:
+        answer = process_file(f)
+    print(f"THE ANSWER IS: {answer}")
+
+
+def process_file(f) -> int:
+    rope = Rope(number_of_knots=10)
+
+    tail_locations = set()
+    for idx, line in enumerate(f.readlines()):
+        direction, amount = line.split()
+
+        for _ in range(int(amount)):
+            rope.move_one_step(direction)
+            tail = rope.knots[-1]
+            tail_locations.add(tail.get_location())
+    return len(tail_locations)
+
+
+if __name__ == '__main__':
+    main()
