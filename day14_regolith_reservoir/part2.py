eec134e54df084b0874cbb6f99b153994c89cbec
Martin Winkel <martin.winkel.pps@gmail.com>
2022-12-14 19:24:33 +0100

    use named tuple and walrus operator for readability (hopefully)

--- day14_regolith_reservoir/part2.py
+++ day14_regolith_reservoir/part2.py
@@ -1,9 +1,10 @@
 import itertools
+from collections import namedtuple
 
 
-Point = tuple[int, int]
+Point = namedtuple("Point", ["x", "y"])
 
-SAND_SOURCE = (500, 0)
+SAND_SOURCE = Point(500, 0)
 
 
 def main():
@@ -28,21 +29,21 @@ def parse_file(f) -> set[Point]:
 
 def parse_coordinates(text: str) -> Point:
     x, y = text.split(",")
-    return int(x), int(y)
+    return Point(int(x), int(y))
 
 
 def points_from_path(path_coordinates: list[Point]) -> set[Point]:
     points = set()
     for point1, point2 in pairwise(path_coordinates):
-        min_x, max_x = min(point1[0], point2[0]), max(point1[0], point2[0])
-        min_y, max_y = min(point1[1], point2[1]), max(point1[1], point2[1])
+        min_x, max_x = min(point1.x, point2.x), max(point1.x, point2.x)
+        min_y, max_y = min(point1.y, point2.y), max(point1.y, point2.y)
         for x_offset, y_offset in itertools.product(
             range(max_x - min_x + 1) or [min_x],
             range(max_y - min_y + 1) or [min_y],
         ):
             x = min_x + x_offset
             y = min_y + y_offset
-            points.add((x, y))
+            points.add(Point(x, y))
     return points
 
 
@@ -54,40 +55,40 @@ def pairwise(iterable):
 
 
 def simulate_sand_flow(rock_points: set[Point]) -> int:
-    max_rock_y = max([point[1] for point in rock_points])
+    max_rock_y = max([point.y for point in rock_points])
     bottom_y = max_rock_y + 2
     num_rocks = len(rock_points)
 
     blocked_points = rock_points.copy()
     current_sand = SAND_SOURCE
     while SAND_SOURCE not in blocked_points:
-        if not is_blocked((current_sand[0], current_sand[1] + 1), blocked_points, bottom_y):
+        if not is_blocked(temp := Point(current_sand.x, current_sand.y + 1), blocked_points, bottom_y):
             # straight down
-            current_sand = (current_sand[0], current_sand[1] + 1)
-        elif not is_blocked((current_sand[0] - 1, current_sand[1] + 1), blocked_points, bottom_y):
+            current_sand = temp
+        elif not is_blocked(temp := Point(current_sand.x - 1, current_sand.y + 1), blocked_points, bottom_y):
             # diagonally down left
-            current_sand = (current_sand[0] - 1, current_sand[1] + 1)
-        elif not is_blocked((current_sand[0] + 1, current_sand[1] + 1), blocked_points, bottom_y):
+            current_sand = temp
+        elif not is_blocked(temp := Point(current_sand.x + 1, current_sand.y + 1), blocked_points, bottom_y):
             # diagonally down right
-            current_sand = (current_sand[0] + 1, current_sand[1] + 1)
+            current_sand = temp
         else:
             # sand can't move, so it comes to a rest
             blocked_points.add(current_sand)
             current_sand = SAND_SOURCE
-    visualise(rock_points, blocked_points)
+    # visualise(rock_points, blocked_points)
     return len(blocked_points) - num_rocks
 
 
 def is_blocked(point, blocked_points, bottom_y):
-    return (point in blocked_points) or (point[1] >= bottom_y)
+    return (point in blocked_points) or (point.y >= bottom_y)
 
 
 def visualise(rock_points, blocked_points):
     sand_rest_points = blocked_points - rock_points
     all_points = blocked_points | {SAND_SOURCE}
 
-    x_coordinates = [p[0] for p in all_points]
-    y_coordinates = [p[1] for p in all_points]
+    x_coordinates = [p.x for p in all_points]
+    y_coordinates = [p.y for p in all_points]
     min_x, max_x = min(x_coordinates), max(x_coordinates)
     min_y, max_y = min(y_coordinates), max(y_coordinates)
 
