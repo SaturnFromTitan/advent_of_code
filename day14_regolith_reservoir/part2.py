86a6cc9df1a4faf3927243249a277bf9d8573942
Martin Winkel <martin.winkel.pps@gmail.com>
2022-12-14 18:49:53 +0100

    part2 of day14. + visualisation function for day14

--- day14_regolith_reservoir/part2.py
+++ day14_regolith_reservoir/part2.py
@@ -1,7 +1,10 @@
+import functools
 import itertools
 
 
 Point = tuple[int, int]
+
+SAND_SOURCE = (500, 0)
 
 
 def main():
@@ -53,32 +56,42 @@ def pairwise(iterable):
 
 def simulate_sand_flow(rock_points: set[Point]) -> int:
     max_rock_y = max([point[1] for point in rock_points])
-    sand_source = (500, 0)
+    bottom_y = max_rock_y + 2
 
     sand_rest_points: set[Point] = set()
-    current_sand = sand_source
-    while current_sand[1] <= max_rock_y:
-        blocked_points = rock_points | sand_rest_points
-        if (current_sand[0], current_sand[1] + 1) not in blocked_points:
+    current_sand = SAND_SOURCE
+    while SAND_SOURCE not in sand_rest_points:
+        _is_blocked = functools.partial(
+            is_blocked,
+            rock_points=rock_points,
+            sand_rest_points=sand_rest_points,
+            bottom_y=bottom_y
+        )
+
+        if not _is_blocked((current_sand[0], current_sand[1] + 1)):
             # straight down
             current_sand = (current_sand[0], current_sand[1] + 1)
-        elif (current_sand[0] - 1, current_sand[1] + 1) not in blocked_points:
+        elif not _is_blocked((current_sand[0] - 1, current_sand[1] + 1)):
             # diagonally down left
             current_sand = (current_sand[0] - 1, current_sand[1] + 1)
-        elif (current_sand[0] + 1, current_sand[1] + 1) not in blocked_points:
+        elif not _is_blocked((current_sand[0] + 1, current_sand[1] + 1)):
             # diagonally down right
             current_sand = (current_sand[0] + 1, current_sand[1] + 1)
         else:
             # sand can't move, so it comes to a rest
             sand_rest_points.add(current_sand)
-            current_sand = sand_source
-
-    visualise(rock_points, sand_rest_points)
+            current_sand = SAND_SOURCE
+    visualise(rock_points, set(sand_rest_points))
     return len(sand_rest_points)
 
 
-def visualise(rock_points, sand_rest_points):
+def is_blocked(point, rock_points, sand_rest_points, bottom_y):
     blocked_points = rock_points | sand_rest_points
+    return (point in blocked_points) or (point[1] >= bottom_y)
+
+
+def visualise(rock_points, sand_rest_points):
+    blocked_points = rock_points | sand_rest_points | {SAND_SOURCE}
     x_coordinates = [p[0] for p in blocked_points]
     y_coordinates = [p[1] for p in blocked_points]
     min_x, max_x = min(x_coordinates), max(x_coordinates)
@@ -96,6 +109,8 @@ def visualise(rock_points, sand_rest_points):
             chars += "#"
         elif point in sand_rest_points:
             chars += "o"
+        elif point == SAND_SOURCE:
+            chars += "+"
         else:
             chars += "."
 
