0b6d7dbb524652c68fc1bcf6900cf30e7a2f8f9d
Martin Winkel <martin.winkel.pps@gmail.com>
2022-12-21 09:19:21 +0100

    part2 of day21

---
+++ day21_monkey_math/part2.py
@@ -0,0 +1,110 @@
+import operator
+from typing import Optional
+
+OPERATORS = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv,
+}
+INVERSE_OPERATORS = {
+    "+": operator.sub,
+    "-": operator.add,
+    "*": operator.truediv,
+    "/": operator.mul,
+}
+INPUT_FILE = "input.txt"
+
+
+def main():
+    with open(INPUT_FILE) as f:
+        raw_jobs = parse_file(f)
+
+    answer = solve_riddle(raw_jobs)
+    print(f"THE ANSWER IS: {answer}")
+
+
+def parse_file(f) -> dict[str, str]:
+    raw_jobs = dict()
+    for line in f.readlines():
+        monkey_id, raw_job = line.strip().split(": ")
+        raw_jobs[monkey_id] = raw_job
+    return raw_jobs
+
+
+def solve_riddle(raw_jobs):
+    return find_humn_value(raw_jobs, "root", result_needed=None)
+
+
+def find_humn_value(raw_jobs: dict[str, str], monkey_id: str, result_needed: Optional[int]) -> int:
+    if monkey_id == "humn":
+        return result_needed
+
+    raw_job = raw_jobs[monkey_id]
+    monkey1_id, operator_raw, monkey2_id = raw_job.split()
+
+    failed_monkey_id = None
+    try:
+        res1 = get_result(raw_jobs, monkey1_id)
+    except ValueError:
+        res1 = None
+        failed_monkey_id = monkey1_id
+
+    try:
+        res2 = get_result(raw_jobs, monkey2_id)
+    except ValueError:
+        res2 = None
+        failed_monkey_id = monkey2_id
+
+    if failed_monkey_id is None:
+        raise LookupError("failed_monkey_id isn't set")
+
+    if result_needed is not None:
+        result_needed = get_new_result_needed(res1, res2, result_needed, operator_raw)
+    else:
+        result_needed = res1 if res1 is not None else res2
+    return find_humn_value(raw_jobs, failed_monkey_id, result_needed)
+
+
+def get_result(raw_jobs: dict[str, str], monkey_id: str) -> int:
+    if monkey_id == "humn":
+        raise ValueError("That's the question...")
+
+    raw_job = raw_jobs[monkey_id]
+    if raw_job.isnumeric():
+        return int(raw_job)
+
+    monkey1_id, operation_raw, monkey2_id = raw_job.split()
+    operation = OPERATORS[operation_raw]
+
+    value1 = get_result(raw_jobs, monkey1_id)
+    value2 = get_result(raw_jobs, monkey2_id)
+    return operation(value1, value2)
+
+
+def get_new_result_needed(val1, val2, result_needed, operator_raw):
+    op = OPERATORS[operator_raw]
+    inverse_op = INVERSE_OPERATORS[operator_raw]
+
+    commutative_operators = {"+", "*"}
+    if operator_raw in commutative_operators:
+        # e.g.
+        #     result_needed == val1 + val2
+        # <=> result_needed - val2 == val1
+        value = val1 if val1 is not None else val2
+        return inverse_op(result_needed, value)
+
+    if val1 is None:
+        # e.g.
+        #     result_needed == val1 / val2
+        # <=> result_needed * val2 == val1
+        return inverse_op(result_needed, val2)
+    else:
+        # e.g.
+        #     result_needed == val1 / val2
+        # <=> val2 = val1 / result_needed
+        return op(val1, result_needed)
+
+
+if __name__ == "__main__":
+    main()
