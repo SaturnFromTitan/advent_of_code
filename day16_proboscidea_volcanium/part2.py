e474307312981ebb057aeffe9931add3689b77a7
Martin Winkel <martin.winkel.pps@gmail.com>
2022-12-26 00:54:20 +0100

    outlined a possible solution for part2 of day16

--- day16_proboscidea_volcanium/part2.py
+++ day16_proboscidea_volcanium/part2.py
@@ -1,1 +1,11 @@
+"""
+Idea, inspired by the reddit thread:
+
+Run the simulation from part1, but store all possible (sub) "paths" for 24 minutes and the
+total pressure they would release. I.e. also include paths that only open 0, 1, etc. valves.
+
+Now find distinct pairs of paths and add their pressure numbers.
+
+The maximum is the result.
+"""
 # TODO
