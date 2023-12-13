6a0c6f7b96e414e296efea4e049706d0bb2d9d57
Martin Winkel <martin@pythomation.de>
2023-12-13 22:49:06 +0100

    feat: day 13 - part 2

--- 2023/day13/part2.py
+++ 2023/day13/part2.py
@@ -15,24 +15,13 @@ def parse_file(f) -> list[str]:
 def score(pattern: str) -> int:
     rows = pattern.split("\n")
     double_rows = check_rows(rows)
+    if double_rows:
+        return 100 * double_rows
+
     double_columns = check_columns(rows)
-    return 100 * double_rows + double_columns
-
-
-def check_rows(lines: list[str]) -> int:
-    reflection_candidates = []
-    for index, (row1, row2) in enumerate(itertools.pairwise(lines)):
-        if row1 == row2:
-            reflection_candidates.append(index)
-
-    for idx in reflection_candidates:
-        length = min(len(lines) - 1 - idx, idx + 1)
-        if all(
-            lines[idx - offset] == lines[idx + 1 + offset]
-            for offset in range(1, length)
-        ):
-            return idx + 1
-    return 0
+    if double_columns:
+        return double_columns
+    raise ValueError(f"Didn't find a reflexion for this pattern\n{pattern}")
 
 
 def check_columns(rows: list[str]) -> int:
@@ -40,5 +29,51 @@ def check_columns(rows: list[str]) -> int:
     return check_rows(columns)
 
 
+def check_rows(lines: list[str]) -> int:
+    reflection_candidates = _get_reflexion_candidates(lines)
+
+    for idx, used_variant_for_candidate, _ in reflection_candidates:
+        used_variant = used_variant_for_candidate
+        length = min(len(lines) - 1 - idx, idx + 1)
+
+        is_reflexion = True
+        for offset in range(1, length):
+            row1 = lines[idx - offset]
+            row2 = lines[idx + 1 + offset]
+            matches, used_variant_for_offset, _ = compare(
+                row1, row2, allow_variants=not used_variant
+            )
+            used_variant = used_variant or used_variant_for_offset
+            if not matches:
+                is_reflexion = False
+
+        if is_reflexion and used_variant:
+            return idx + 1
+    return 0
+
+
+def _get_reflexion_candidates(lines: list[str]) -> list[tuple[int, bool, str]]:
+    reflection_candidates = []
+    for index, (row1, row2) in enumerate(itertools.pairwise(lines)):
+        matches, with_variant, row1_variant = compare(row1, row2, allow_variants=True)
+        if matches:
+            reflection_candidates.append((index, with_variant, row1_variant))
+    return reflection_candidates
+
+
+def compare(line1: str, line2: str, allow_variants: bool) -> tuple[bool, bool, str]:
+    if line1 == line2:
+        return True, False, line1
+
+    if allow_variants:
+        for char_index, char in enumerate(line1):
+            new_char = "." if char == "#" else "#"
+            clean_line1 = line1[:char_index] + new_char + line1[char_index + 1 :]
+            if clean_line1 == line2:
+                return True, True, clean_line1
+    return False, False, line1
+
+
 if __name__ == "__main__":
-    main("example_input.txt")
+    # must be higher than 38_726
+    main("input.txt")
