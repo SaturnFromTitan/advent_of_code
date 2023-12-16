a65f68a525404f520b94011c171ae96dd086c93f
Martin Winkel <martin@pythomation.de>
2023-12-16 14:58:21 +0100

    feat: day 16 - part 2

--- 2023/day16/part2.py
+++ 2023/day16/part2.py
@@ -24,14 +24,38 @@ def parse_file(f) -> list[str]:
 
 
 def simulate_ray(grid: list[str]) -> int:
-    current_ray_positions = {(0, -1, Direction.RIGHT)}
+    max_row = len(grid) - 1
+    max_col = len(grid[0]) - 1
+
+    max_score = 0
+    for row in range(max_row + 1):
+        score_right = simulate_ray_with_start(grid, start=(row, -1, Direction.RIGHT))
+        score_left = simulate_ray_with_start(
+            grid, start=(row, max_col + 1, Direction.LEFT)
+        )
+        max_score = max(max_score, score_right, score_left)
+
+    for col in range(max_col + 1):
+        score_down = simulate_ray_with_start(grid, start=(-1, col, Direction.DOWN))
+        score_up = simulate_ray_with_start(grid, start=(max_row + 1, col, Direction.UP))
+        max_score = max(max_score, score_down, score_up)
+
+    return max_score
+
+
+def simulate_ray_with_start(grid: list[str], start: RayPosition) -> int:
+    print(f"simulation with start '{start}'")
+    current_ray_positions = {start}
     ray_positions = current_ray_positions.copy()
     previous_ray_positions: set[RayPosition] = set()
     while ray_positions != previous_ray_positions:
         previous_ray_positions = ray_positions.copy()
         current_ray_positions = move_ray(current_ray_positions, grid)
         ray_positions |= current_ray_positions
-    activated_tiles = {(row, col) for (row, col, _) in ray_positions} - {(0, -1)}
+    activated_tiles = {(row, col) for (row, col, _) in ray_positions} - {
+        (start[0], start[1])
+    }
+    print(f"\tactivated tiles: {len(activated_tiles)}")
     return len(activated_tiles)
 
 
@@ -42,7 +66,10 @@ def move_ray(ray_positions: set[RayPosition], grid: list[str]) -> set[RayPositio
     new_ray_positions = set()
     for pos in ray_positions:
         row, col, _ = pos
-        symbol = grid[row][col]
+        if is_on_grid(row, col, max_row, max_col):
+            symbol = grid[row][col]
+        else:
+            symbol = "."  # start positions are off-grid
         new_ray_positions |= get_new_positions(symbol, pos, max_row, max_col)
     return new_ray_positions
 
@@ -102,9 +129,13 @@ def get_new_positions(
     return {
         (row + row_offset, col + col_offset, new_direction)
         for (row_offset, col_offset, new_direction) in offsets
-        if 0 <= row + row_offset <= max_row and 0 <= col + col_offset <= max_col
+        if is_on_grid(row + row_offset, col + col_offset, max_row, max_col)
     }
 
 
+def is_on_grid(row: int, col: int, max_row: int, max_col: int) -> bool:
+    return 0 <= row <= max_row and 0 <= col <= max_col
+
+
 if __name__ == "__main__":
-    main("example_input.txt")
+    main("input.txt")
